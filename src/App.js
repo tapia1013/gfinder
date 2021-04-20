39da603a8d086499c157a39d6c391a7f7e3ded48
tapia1013 <manbearpig1013@gmail.com>
2021-04-19 18:09:06 -0700

    done with project... starting refactor

--- src/App.js
+++ src/App.js
@@ -16,7 +16,8 @@ class App extends Component {
     users: [],
     user: {},
     loading: false,
-    alert: null
+    alert: null,
+    repos: []
   }
 
   // async componentDidMount() {
@@ -55,6 +56,15 @@ class App extends Component {
     this.setState({ user: res.data, loading: false })
   }
 
+  // Get users repos
+  getUserRepos = async (username) => {
+    this.setState({ loading: true })
+
+    const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
+
+    this.setState({ repos: res.data, loading: false })
+  }
+
 
   // Clear users from state
   clearUsers = () => this.setState({ users: [], loading: false });
@@ -72,7 +82,7 @@ class App extends Component {
 
 
   render() {
-    const { users, loading, user } = this.state;
+    const { users, loading, user, repos } = this.state;
 
     return (
       <Router>
@@ -100,7 +110,14 @@ class App extends Component {
 
               <Route exact path="/about" component={About} />
               <Route exact path='/user/:login' render={props => (
-                <User {...props} getUser={this.getUser} user={user} loading={loading} />
+                <User
+                  {...props}
+                  getUser={this.getUser}
+                  getUserRepos={this.getUserRepos}
+                  user={user}
+                  repos={repos}
+                  loading={loading}
+                />
               )} />
             </Switch>
 
--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,12 +1,108 @@
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react';
+import Spinner from '../layout/Spinner';
+import Repos from '../repos/Repos';
+import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
+
 
 class User extends Component {
+  componentDidMount() {
+    this.props.getUser(this.props.match.params.login);
+    this.props.getUserRepos(this.props.match.params.login);
+  }
+
+  static propTypes = {
+    loading: PropTypes.bool,
+    user: PropTypes.object.isRequired,
+    getUser: PropTypes.func.isRequired,
+    getUserRepos: PropTypes.func.isRequired,
+    repos: PropTypes.array.isRequired
+  }
+
   render() {
+    const { name, avatar_url, location, bio, blog, login, html_url, company, followers, following, public_repos, public_gists, hireable } = this.props.user;
+
+    const { loading, repos } = this.props;
+
+    // if loading is true show spinner
+    if (loading) return <Spinner />
+
     return (
-      <div>
-        user
-      </div>
+      <Fragment>
+        <Link className="btn btn-light" to="/">
+          Back To Search
+        </Link>
+        Hireable: {' '}
+        {
+          hireable ?
+            <i className="fas fa-check text-success" /> :
+            <i className="fas fa-times-circle text-danger" />
+        }
+        <div className="card grid-2">
+          <div className="all-center">
+            <img
+              src={avatar_url}
+              className="round-img"
+              style={{ width: '150px' }}
+            />
+            <h1>{name}</h1>
+            <p>Location: {location}</p>
+          </div>
+          <div className="">
+            {bio && (
+              <Fragment>
+                <h3>Bio</h3>
+                <p>{bio}</p>
+              </Fragment>
+            )}
+            <a className="btn btn-dark my-1" href={html_url}>Visit Github Profile</a>
+            <ul>
+              <li>{
+                login && (
+                  <Fragment>
+                    <strong>Username: </strong> {login}
+                  </Fragment>
+                )
+              }</li>
+              <li>{
+                company && (
+                  <Fragment>
+                    <strong>Company: </strong> {company}
+                  </Fragment>
+                )
+              }</li>
+              <li>{
+                blog && (
+                  <Fragment>
+                    <strong>Website: </strong> {blog}
+                  </Fragment>
+                )
+              }</li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="card text-center">
+          <div className="badge badge-primary">
+            Followers: {followers}
+          </div>
+          <div className="badge badge-success">
+            Following: {following}
+          </div>
+          <div className="badge badge-light">
+            Public Repos: {public_repos}
+          </div>
+          <div className="badge badge-dark">
+            Public Gists: {public_gists}
+          </div>
+        </div>
+
+        <Repos repos={repos} />
+      </Fragment>
     )
   }
 }
 export default User;
+
+
+// section 5 refactoring
