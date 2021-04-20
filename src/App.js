88706a0f4306f0e2c623e7252ce7f0693f58ca7a
tapia1013 <manbearpig1013@gmail.com>
2021-04-20 16:44:02 -0700

    done with context ready for deployment

--- src/App.js
+++ src/App.js
@@ -1,113 +1,56 @@
-import React, { useState, Fragment } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users';
-import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
 import User from './components/users/User';
-import axios from 'axios';
+import Home from './components/pages/Home';
+import NotFound from './components/pages/NotFound';
+
+
+import GithubState from './context/github/GithubState';
+import AlertState from './context/alert/AlertState';
+
 
 import './App.css';
 
 
 const App = () => {
-  const [users, setUsers] = useState([])
-  const [user, setUser] = useState({})
-  const [loading, setLoading] = useState(false)
-  const [alert, setAlert] = useState(null)
-  const [repos, setRepos] = useState([])
-
-
-
-  // Search github users...so we dont need a componentDidMount so itll start off blank and search will populate
-  const searchUsers = async (text) => {
-    // console.log(text);
-    setLoading(true)
-
-    const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
-
-    setUsers(res.data.items)
-    setLoading(false)
-  }
-
-
-  // Get single GutHubuser
-  const getUser = async (username) => {
-    setLoading(true)
-
-    const res = await axios.get(`https://api.github.com/users/${username}?&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
-
-    setUser(res.data)
-    setLoading(false)
-  }
-
-
-  // Get users repos
-  const getUserRepos = async (username) => {
-    setLoading(true)
-
-    const res = await axios.get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`)
-
-    setRepos(res.data)
-    setLoading(false)
-  }
-
-
-  // Clear users from state
-  const clearUsers = () => {
-    setUsers([])
-    setLoading(false)
-  }
-
-
-  // Set alert
-  const showAlert = (msg, type) => {
-    setAlert({ msg, type })
-    setTimeout(() => setAlert(null), 3000)
-  }
-
 
   return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <div className="container">
-          <Alert alert={alert} />
-          <Switch>
-            <Route exact path="/" render={props => (
-              <Fragment>
-                <Search
-                  searchUsers={searchUsers}
-                  clearUsers={clearUsers}
-                  showClear={
-                    users.length > 0 ? true : false
-                  }
-                  setAlert={showAlert}
+    <GithubState>
+      <AlertState>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <div className="container">
+              <Alert />
+              <Switch>
+                <Route
+                  exact
+                  path="/"
+                  component={Home}
                 />
-                <Users
-                  loading={loading}
-                  users={users}
+                <Route
+                  exact
+                  path="/about"
+                  component={About}
                 />
-              </Fragment>
-            )} />
+                <Route
+                  exact
+                  path='/user/:login'
+                  component={User}
+                />
+                <Route
+                  component={NotFound}
+                />
+              </Switch>
 
-            <Route exact path="/about" component={About} />
-            <Route exact path='/user/:login' render={props => (
-              <User
-                {...props}
-                getUser={getUser}
-                getUserRepos={getUserRepos}
-                user={user}
-                repos={repos}
-                loading={loading}
-              />
-            )} />
-          </Switch>
-
-        </div>
-      </div>
-    </Router>
+            </div>
+          </div>
+        </Router>
+      </AlertState>
+    </GithubState>
   )
 }
 
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,16 +1,20 @@
-import React, { useState } from 'react';
-import PropTypes from 'prop-types';
+import React, { useState, useContext } from 'react';
+import GithubContext from '../../context/github/githubContext';
+import AlertContext from '../../context/alert/alertContext';
 
 
-const Search = ({ searchUsers, showClear, clearUsers, setAlert }) => {
+const Search = () => {
+  const githubContext = useContext(GithubContext);
+  const alertContext = useContext(AlertContext);
+
   const [text, setText] = useState('')
 
   const onSubmit = (e) => {
     e.preventDefault();
     if (text === '') {
-      setAlert('Please enter something', 'light')
+      alertContext.setAlert('Please enter something', 'light')
     } else {
-      searchUsers(text)
+      githubContext.searchUsers(text)
       // resets text to nothing
       setText('')
     }
@@ -37,10 +41,10 @@ const Search = ({ searchUsers, showClear, clearUsers, setAlert }) => {
         />
       </form>
       {
-        showClear && (
+        githubContext.users.length > 0 && (
           <button
             className="btn btn-light btn-block"
-            onClick={clearUsers}
+            onClick={githubContext.clearUsers}
           >
             Clear
           </button>
@@ -50,12 +54,5 @@ const Search = ({ searchUsers, showClear, clearUsers, setAlert }) => {
   )
 }
 
-
-Search.propTypes = {
-  searchUsers: PropTypes.func.isRequired,
-  clearUsers: PropTypes.func.isRequired,
-  showClear: PropTypes.bool.isRequired,
-  setAlert: PropTypes.func.isRequired
-}
 
 export default Search
